c0f8dd3a98cc55ddad296b384f578325c798c258
Minh Duc Ta <ductm310@gmail.com>
2015-06-29 23:38:32 +0700

    current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,21 @@ module SessionsHelper
     session[:token] = Ckfapi::API::User.get_token(email, password)
   end
 
+  def remove_token(token)
+    dtoken = Ckfapi::API::User.remove_token(token)
+    session[:token] = nil if dtoken["message"].include? "Session deleted"
+    session[:token]
+  end
+
   def current_token
     session[:token]
+  end
+
+  def current_user
+    session[:user] ||= Ckfapi::API::User.index(
+      current_token,
+      {email: current_token["user"]["email"] }
+    )["users"][0] rescue nil
   end
 
 end
