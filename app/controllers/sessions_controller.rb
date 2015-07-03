e64d3e48742860815c1d18a81e31ec418f748128
Minh Duc Ta <ductm310@gmail.com>
2015-07-03 11:05:57 +0700

    after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
 
   def create
     @token = get_token(params[:email], params[:password])
-    if @token["user"]
-      redirect_to root_path
+    if @token["token"]
+      redirect_to get_root_path(current_user)
     else
       redirect_to new_sessions_path
     end
