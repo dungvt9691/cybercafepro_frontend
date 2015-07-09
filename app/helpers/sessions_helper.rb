99226ecc031d5cc408369023b44bedb7632b0812
Minh Duc Ta <ductm310@gmail.com>
2015-07-09 16:30:30 +0700

    merged

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,12 +13,12 @@ module SessionsHelper
   end
 
   def current_token
-    # session[:token]
-    if mobile_device?
-      Ckfapi::API::User.get_token("dungvt9691@gmail.com", 123123123)
-    else
-      Ckfapi::API::User.get_token("doankien.bui@gmail.com", 123123123)
-    end
+    session[:token]
+    # if mobile_device?
+      # Ckfapi::API::User.get_token("dungvt9691@gmail.com", 123123123)
+    # else
+      # Ckfapi::API::User.get_token("doankien.bui@gmail.com", 123123123)
+    # end
   end
 
   def current_user
