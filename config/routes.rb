5d42952b41d4a2041ec6d9f08e9ca5390df1abe4
Minh Duc Ta <ductm310@gmail.com>
2015-07-17 10:18:35 +0700

    shift update 1

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'customer_pages#customer_ordering'
 
-  resource :users
-  resource :sessions
+  resources :users
+  resource :sessions, only: [:new, :create, :destroy]
+  resources :registrations
 
   resources :customer_pages,only: [] do
     collection do
