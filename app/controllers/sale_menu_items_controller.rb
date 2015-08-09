b6ab95e88275935fbed06434e63a1fc0576d0d10
Minh Duc Ta <ductm310@gmail.com>
2015-08-09 19:51:27 +0700

    Customer can remove sale_menu_item

--- app/controllers/sale_menu_items_controller.rb
+++ app/controllers/sale_menu_items_controller.rb
@@ -11,7 +11,9 @@ class SaleMenuItemsController < ApplicationController
 
   def destroy
     @sale_menu_item_id = params[:id]
+    sale_menu_item = Ckfapi::API::SaleMenuItem.get(current_token, params[:id], {detail: true})
     @sale_menu_item = Ckfapi::API::SaleMenuItem.remove(current_token, params[:id])
+    WebsocketRails[:staff].trigger 'destroy_sale_menu_item', sale_menu_item if @sale_menu_item.blank?
     respond_to do |format|
       format.js
     end
@@ -20,7 +22,7 @@ class SaleMenuItemsController < ApplicationController
   private
 
   def filter_role
-    return true if current_user && ["Manager"].include?(current_user['current_role'])
+    return true if current_user && ["Manager","Customer"].include?(current_user['current_role'])
     flash[:error] = "Can only be acessed by Manager"
     redirect_to get_root_path(current_user)
   end
